1d85d841538d76ac1b672c8c6b43122b1f80bea3
Jeki Maulana <jekibus@gmail.com>
2024-03-15 09:00:14 +0700

    Create template widgets

---
+++ src/lib/components/widgets/template.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let config: any;
+</script>
+
+<div class="bg-gray-100 p-5">
+	<h1 class="text-lg text-blue-500">{config.title}</h1>
+	<h2 class="text-sm text-gray-500">{config.subtitle}</h2>
+</div>
+<!-- {JSON.stringify(grid)} -->
