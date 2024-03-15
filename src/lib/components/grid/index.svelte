1841ac88d096241b3d053a27aa19128ac37e6311
Jeki Maulana <jekibus@gmail.com>
2024-03-15 15:37:38 +0700

    Add grid logic: flex and grid

--- src/lib/components/grid/index.svelte
+++ src/lib/components/grid/index.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-	import { convertSlash } from '$lib/helper';
+	import Items from './items.svelte';
 
 	export let data: any;
+	export let schemas: any = undefined;
 </script>
 
 {#if data && data.grid && data.grid.length > 0}
-	<div
-		class="grid {data?.options?.no_gap ? '' : 'gap-4'} grid-cols-1 sm:grid-cols-2 lg:grid-cols-4"
-	>
-		{#each data.grid as item}
-			<div class={item.class}>
-				{#if data.components[convertSlash(item.component)]}
-					<svelte:component
-						this={data.components[convertSlash(item.component)]}
-						config={item.config}
-					/>
-				{/if}
-			</div>
-		{/each}
-	</div>
+	{#if data.options?.grid === false}
+		<div class="flex h-screen items-center justify-center">
+			<Items {data} {schemas} />
+		</div>
+	{:else}
+		<div
+			class="grid {data.options?.gap !== false
+				? 'gap-4'
+				: ''} grid-cols-1 sm:grid-cols-2 lg:grid-cols-4"
+		>
+			<Items {data} {schemas} />
+		</div>
+	{/if}
 {:else}
 	<p>No config found. Please contact your Administrator.</p>
 {/if}
---
+++ src/lib/components/grid/items.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { convertSlash } from '$lib/helper';
+
+	export let data: any;
+	export let schemas: any;
+</script>
+
+{#each data.grid as item}
+	{@const config = { ...item.config, schema: schemas[item.config?.schema] }}
+	<div class={item.class}>
+		{#if data.components[convertSlash(item.component)]}
+			<svelte:component
+				this={data.components[convertSlash(item.component)]}
+				{config}
+				data={item.data}
+			/>
+		{/if}
+	</div>
+{/each}
