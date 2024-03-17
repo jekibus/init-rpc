e305ec84b37e0e40f971f3221c76742d6b2836d2
Jeki Maulana <jekibus@gmail.com>
2024-03-18 06:17:14 +0700

    Update site config to setup version showing

--- src/lib/configs/site.ts
+++ src/lib/configs/site.ts
@@ -2,7 +2,8 @@ import {
 	PUBLIC_BASE_URL,
 	PUBLIC_LAST_COMMIT,
 	PUBLIC_LAST_COMMIT_TS,
-	PUBLIC_TAG
+	PUBLIC_TAG,
+	PUBLIC_SHOW_VERSION
 } from '$env/static/public';
 
 export const siteConfig = {
@@ -11,6 +12,7 @@ export const siteConfig = {
 	ogImage: `${PUBLIC_BASE_URL}/og.png`,
 	description: 'Outcome Based Education di Fisika UIN Bandung',
 	keywords: `bosgrid,bosapi,merdika,obe`,
+	showVersion: PUBLIC_SHOW_VERSION === 'true' || false,
 	version: PUBLIC_TAG,
 	lastCommit: PUBLIC_LAST_COMMIT,
 	lastCommitTs: PUBLIC_LAST_COMMIT_TS
