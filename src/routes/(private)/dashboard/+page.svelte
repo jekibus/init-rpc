03d2cf8280782e256ade9e1ca082db915c33d4ee
Jeki Maulana <jekibus@gmail.com>
2024-03-18 06:37:29 +0700

    Initilalization dashboard

---
+++ src/routes/(private)/dashboard/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+</script>
+
+<p>
+	This is your dashboard page This is your dashboard page This is your dashboard page This is your
+	dashboard page This is your dashboard page This is your dashboard page
+</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
+<p>This is your dashboard page</p>
