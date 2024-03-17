b0df25916d3968b7bb617b40edcb9f7df5059fd6
Jeki Maulana <jekibus@gmail.com>
2024-03-18 06:42:28 +0700

    Update helper fsm components

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,8 +1,8 @@
 import * as components from '$lib/components/index';
 
-export function convertSlash(slash: string) {
-	return slash?.replace(/\//g, '_');
-}
+// export function convertSlash(slash: string) {
+// 	return slash?.replace(/\//g, '_');
+// }
 
 export const cloneObject = (o: any) => {
 	return JSON.parse(JSON.stringify(o));
@@ -12,7 +12,7 @@ export const getComponents = async (grid: any[] = []): Promise<any> => {
 	if (grid.length === 0) return {};
 	const componentList: any = {};
 	for (let index = 0; index < grid.length; index++) {
-		const componentName = convertSlash(grid[index].component);
+		const componentName = grid[index].component;
 		if (componentList[componentName] === undefined) {
 			// @ts-ignore
 			componentList[componentName] = components[componentName];
